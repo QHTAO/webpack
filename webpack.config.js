7daffae675b3d050f0bb6279a8d0406be20fa5ec
ht <hunterchiu@qq.com>
2020-11-26 22:54:01 +0800

    使用 JSX

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,18 @@ module.exports = {
     path: path.resolve(__dirname, "/dist"),
     filename: "[name].js",
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env","@babel/preset-react"],
+          },
+        },
+      },
+    ],
+  },
 };
